2fe09c60f562d0595d77d291c533a58cbeb63cb4
墨綠BG <s5460703@gmail.com>
2024-06-05 17:24:18 +0800

    ✨ feat：新增字体

---
+++ src/common/components/layouts/index.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+interface LayoutProps {
+  children: React.ReactNode;
+}
+
+const Layout: React.FC<LayoutProps> = React.memo(({ children }) => {
+  return <>{children}</>;
+});
+
+export default Layout;
