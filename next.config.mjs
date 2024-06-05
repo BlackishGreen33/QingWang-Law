9f5d9efa0147156ef68d6994cb676b9d2bd031a9
墨綠BG <s5460703@gmail.com>
2024-06-05 16:58:25 +0800

    🔧 chore：配置 Next.js 设定

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,24 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+
+const nextConfig = {
+  reactStrictMode: true,
+  compiler: {
+    styledComponents: true,
+  },
+  swcMinify: true,
+  // output: "export",
+  images: {
+    unoptimized: true,
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'www.github.com',
+        port: '',
+        pathname: '/**',
+      },
+    ],
+    formats: ['image/avif', 'image/webp'],
+  },
+};
 
 export default nextConfig;
