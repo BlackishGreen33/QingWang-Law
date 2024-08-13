ba2df399508bfd1abdbb2af66338b3c687920609
墨綠BG <s5460703@gmail.com>
2024-08-13 21:13:18 +0800

    🔨 refactor：封装表单文本输入框组件

--- src/module/Login/components/Login.tsx
+++ src/module/Login/components/Login.tsx
@@ -2,45 +2,34 @@
 
 import Link from 'next/link';
 import { useRouter } from 'next/navigation';
-import React from 'react';
+import React, { useState } from 'react';
 
+import AuthInput from '@/common/components/auth/AuthInput';
 import Breakline from '@/common/components/elements/Breakline';
 import { Button } from '@/common/components/ui/button';
 
 const Login: React.FC = React.memo(() => {
   const router = useRouter();
 
+  const [loginForm, setLoginForm] = useState({ email: '', password: '' });
+
   return (
     <div className="flex h-screen flex-col items-center justify-center">
       <p className="text-3xl font-bold">欢迎回来</p>
-      <div className="relative mt-6 w-80">
-        <input
-          type="text"
-          id="small_outlined"
-          className="text-md peer block h-14 w-full appearance-none rounded-lg border-[1.5px] border-gray-300 bg-transparent px-2.5 pb-1.5 pt-3 text-gray-900 focus:border-primary focus:outline-none focus:ring-0 dark:border-gray-600 dark:text-white dark:focus:border-primary"
-          placeholder=""
-        />
-        <label
-          htmlFor="small_outlined"
-          className="absolute start-1 top-1 z-10 flex origin-[0] -translate-y-3 scale-75 transform gap-1 bg-bgDefault px-3 text-sm text-gray-500 duration-300 peer-placeholder-shown:top-1/2 peer-placeholder-shown:-translate-y-1/2 peer-placeholder-shown:scale-100 peer-focus:top-1 peer-focus:mx-2 peer-focus:-translate-y-3 peer-focus:scale-75 peer-focus:px-2 peer-focus:text-primary dark:bg-gray-900 dark:text-gray-400 peer-focus:dark:text-primary rtl:peer-focus:left-auto rtl:peer-focus:translate-x-1/4"
-        >
-          电子邮件地址 <p>*</p>
-        </label>
-      </div>
-      <div className="relative mt-6 w-80">
-        <input
-          type="password"
-          id="small_outlined"
-          className="text-md peer block h-14 w-full appearance-none rounded-lg border-[1.5px] border-gray-300 bg-transparent px-2.5 pb-1.5 pt-3 text-gray-900 focus:border-primary focus:outline-none focus:ring-0 dark:border-gray-600 dark:text-white dark:focus:border-primary"
-          placeholder=""
-        />
-        <label
-          htmlFor="small_outlined"
-          className="absolute start-1 top-1 z-10 flex origin-[0] -translate-y-3 scale-75 transform gap-1 bg-bgDefault px-3 text-sm text-gray-500 duration-300 peer-placeholder-shown:top-1/2 peer-placeholder-shown:-translate-y-1/2 peer-placeholder-shown:scale-100 peer-focus:top-1 peer-focus:mx-2 peer-focus:-translate-y-3 peer-focus:scale-75 peer-focus:px-2 peer-focus:text-primary dark:bg-gray-900 dark:text-gray-400 peer-focus:dark:text-primary rtl:peer-focus:left-auto rtl:peer-focus:translate-x-1/4"
-        >
-          密码 <p>*</p>
-        </label>
-      </div>
+      <AuthInput
+        name="电子邮件地址"
+        type="text"
+        value={loginForm.email}
+        onChange={(e) => setLoginForm({ ...loginForm, email: e.target.value })}
+      />
+      <AuthInput
+        name="密码"
+        type="password"
+        value={loginForm.password}
+        onChange={(e) =>
+          setLoginForm({ ...loginForm, password: e.target.value })
+        }
+      />
       <Button
         className="text-md mt-6 h-14 w-80 bg-primary hover:bg-lightprimary"
         onClick={() => router.push('/')}
