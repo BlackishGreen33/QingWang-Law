b5ef07a17732dc50fe025bdfe8f1ffdba9677d42
墨綠BG <s5460703@gmail.com>
2024-08-13 21:01:18 +0800

    🔨 refactor：更新登录注册页面样式

--- src/module/Login/components/Login.tsx
+++ src/module/Login/components/Login.tsx
@@ -3,8 +3,6 @@
 import Link from 'next/link';
 import { useRouter } from 'next/navigation';
 import React from 'react';
-import { FcGoogle } from 'react-icons/fc';
-import { SiMicrosoft } from 'react-icons/si';
 
 import Breakline from '@/common/components/elements/Breakline';
 import { Button } from '@/common/components/ui/button';
@@ -24,16 +22,30 @@ const Login: React.FC = React.memo(() => {
         />
         <label
           htmlFor="small_outlined"
-          className="absolute start-1 top-1 z-10 origin-[0] -translate-y-3 scale-75 transform bg-bgDefault px-3 text-sm text-gray-500 duration-300 peer-placeholder-shown:top-1/2 peer-placeholder-shown:-translate-y-1/2 peer-placeholder-shown:scale-100 peer-focus:top-1 peer-focus:mx-2 peer-focus:-translate-y-3 peer-focus:scale-75 peer-focus:px-2 peer-focus:text-primary dark:bg-gray-900 dark:text-gray-400 peer-focus:dark:text-primary rtl:peer-focus:left-auto rtl:peer-focus:translate-x-1/4"
+          className="absolute start-1 top-1 z-10 flex origin-[0] -translate-y-3 scale-75 transform gap-1 bg-bgDefault px-3 text-sm text-gray-500 duration-300 peer-placeholder-shown:top-1/2 peer-placeholder-shown:-translate-y-1/2 peer-placeholder-shown:scale-100 peer-focus:top-1 peer-focus:mx-2 peer-focus:-translate-y-3 peer-focus:scale-75 peer-focus:px-2 peer-focus:text-primary dark:bg-gray-900 dark:text-gray-400 peer-focus:dark:text-primary rtl:peer-focus:left-auto rtl:peer-focus:translate-x-1/4"
         >
-          电子邮件地址
+          电子邮件地址 <p>*</p>
+        </label>
+      </div>
+      <div className="relative mt-6 w-80">
+        <input
+          type="password"
+          id="small_outlined"
+          className="text-md peer block h-14 w-full appearance-none rounded-lg border-[1.5px] border-gray-300 bg-transparent px-2.5 pb-1.5 pt-3 text-gray-900 focus:border-primary focus:outline-none focus:ring-0 dark:border-gray-600 dark:text-white dark:focus:border-primary"
+          placeholder=""
+        />
+        <label
+          htmlFor="small_outlined"
+          className="absolute start-1 top-1 z-10 flex origin-[0] -translate-y-3 scale-75 transform gap-1 bg-bgDefault px-3 text-sm text-gray-500 duration-300 peer-placeholder-shown:top-1/2 peer-placeholder-shown:-translate-y-1/2 peer-placeholder-shown:scale-100 peer-focus:top-1 peer-focus:mx-2 peer-focus:-translate-y-3 peer-focus:scale-75 peer-focus:px-2 peer-focus:text-primary dark:bg-gray-900 dark:text-gray-400 peer-focus:dark:text-primary rtl:peer-focus:left-auto rtl:peer-focus:translate-x-1/4"
+        >
+          密码 <p>*</p>
         </label>
       </div>
       <Button
         className="text-md mt-6 h-14 w-80 bg-primary hover:bg-lightprimary"
         onClick={() => router.push('/')}
       >
-        继续
+        登录
       </Button>
       <p className="mt-4 flex text-sm">
         还没有账户？{' '}
@@ -43,13 +55,8 @@ const Login: React.FC = React.memo(() => {
       </p>
       <Breakline className="mt-8 h-[1px] w-80" text="或"></Breakline>
       <Button className="text-md mt-4 h-14 w-80 justify-start border-[1.5px] border-gray-300 bg-transparent text-black shadow-none">
-        <p className="text-md flex items-center gap-4">
-          <FcGoogle className="text-lg" /> 继续使用 Google 登录
-        </p>
-      </Button>
-      <Button className="text-md mt-2 h-14 w-80 justify-start border-[1.5px] border-gray-300 bg-transparent text-black shadow-none">
-        <p className="text-md flex items-center gap-4">
-          <SiMicrosoft className="text-lg" /> 继续使用 Microsoft 账户登录
+        <p className="text-md flex w-full items-center justify-center gap-4 font-bold">
+          Continue with AccessToken
         </p>
       </Button>
     </div>
