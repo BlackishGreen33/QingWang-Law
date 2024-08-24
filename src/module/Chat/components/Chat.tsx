c892db764e12369f0ed5cd54203e125bba9abc85
墨綠BG <s5460703@gmail.com>
2024-08-24 17:35:36 +0800

    ✨ feat：新增聊天接口

--- src/module/Chat/components/Chat.tsx
+++ src/module/Chat/components/Chat.tsx
@@ -2,11 +2,12 @@
 
 import axios from 'axios';
 import Link from 'next/link';
-import React, { useEffect, useState } from 'react';
+import React, { useCallback, useEffect, useState } from 'react';
 import { BsArrowUpSquareFill } from 'react-icons/bs';
 import { FaChevronDown } from 'react-icons/fa';
 
 import { Input } from '@/common/components/ui/input';
+import { cn } from '@/common/utils/utils';
 
 import Record from './Record';
 
@@ -22,35 +23,83 @@ interface ChatProps {
 const Chat: React.FC<ChatProps> = React.memo(({ chat_id }) => {
   const [messages, setMessages] = useState<Message[]>([]);
 
-  useEffect(() => {
-    const token = localStorage.getItem('token');
-    if (!token) {
-      window.location.href = '/login';
-    }
+  const [question, setQuestion] = useState<string>('');
 
-    const getChatrooms = async () => {
-      try {
-        const fetchMessages = await axios.get(
-          `http://127.0.0.1:6006/chat/${chat_id}?chat_id=${chat_id}`,
-          {
-            headers: { Authorization: token as string },
-          }
-        );
+  let token = '';
+  if (typeof window !== 'undefined') {
+    token = localStorage.getItem('token')!;
+  }
 
-        // console.log(fetchMessages.data);
-        // setMessages(fetchMessages.data.chats);
-      } catch (error) {
-        // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-        // @ts-expect-error
-        if (error.response.status === 401) {
-          window.location.href = '/login';
+  const getMessages = useCallback(async () => {
+    try {
+      const fetchMessages = await axios.get(
+        `http://127.0.0.1:6006/chat/${chat_id}?chat_id=${chat_id}`,
+        {
+          headers: { Authorization: token as string },
         }
-        // eslint-disable-next-line no-console
-        console.log(error);
+      );
+      const data = fetchMessages.data.messages.map(
+        (message: { content: string; role: string }) => ({
+          message: message.content,
+          isMe: message.role === 'user',
+        })
+      );
+      setMessages(data);
+      // const dataWithoutLastElement = data.slice(0, -1);
+      // setMessages(dataWithoutLastElement);
+      // const lastElement = data.at(-1);
+      // if (lastElement && lastElement.message.isMe === false) {
+      //   setMessages([...messages, { message: '', isMe: false }]);
+      //   const strToAppend = lastElement.message;
+      //   const appendCharacter = (index = 0) => {
+      //     if (index < strToAppend.length) {
+      //       const newMessages = [...messages];
+      //       newMessages[newMessages.length - 1].message +=
+      //         strToAppend.charAt(index);
+      //       setMessages(newMessages);
+      //       setTimeout(() => {
+      //         appendCharacter(index + 1);
+      //       }, 50);
+      //     }
+      //   };
+      // }
+    } catch (error) {
+      // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+      // @ts-expect-error
+      if (error.response && error?.response.status === 401) {
+        window.location.href = '/login';
       }
-    };
-    getChatrooms();
-  }, [chat_id]);
+      // eslint-disable-next-line no-console
+      console.log(error);
+    }
+  }, [chat_id, token]);
+
+  const sentQuestion = async () => {
+    try {
+      const anwser = await axios.post(
+        `http://127.0.0.1:6006/chat/stream/${chat_id}?chat_id=${chat_id}`,
+        { inputs: question },
+        {
+          headers: { Authorization: token as string },
+        }
+      );
+      setQuestion('');
+      console.log(anwser);
+      getMessages();
+    } catch (error) {
+      // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+      // @ts-expect-error
+      if (error.response.status === 401) {
+        window.location.href = '/login';
+      }
+      // eslint-disable-next-line no-console
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    getMessages();
+  }, [getMessages]);
 
   return (
     <>
@@ -67,9 +116,16 @@ const Chat: React.FC<ChatProps> = React.memo(({ chat_id }) => {
           <Input
             className="h-14 w-full rounded-2xl rounded-r-none border-[1.5px] border-r-0 border-gray-300 pl-6 outline-none"
             placeholder={`给"LAW"发送消息`}
+            value={question}
+            onChange={(e) => setQuestion(e.target.value)}
           />
           <div className="flex h-14 items-center rounded-2xl rounded-l-none border-[1.5px] border-l-0 border-gray-300 pr-4 outline-none">
-            <BsArrowUpSquareFill className="text-3xl text-gray-300" />
+            <BsArrowUpSquareFill
+              className={cn('text-3xl text-gray-300', {
+                'text-gray-500': question,
+              })}
+              onClick={sentQuestion}
+            />
           </div>
         </div>
         <p className="text-sm text-gray-500">
